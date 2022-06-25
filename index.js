8ffe19879965876156b433f9957e543d683f1ce5
Glenn George <ggpersonaltraining@gmail.com>
2022-06-25 23:23:44 +1000

    task item local storage updates

--- index.js
+++ index.js
@@ -20,48 +20,13 @@ let day = days[date.getDay()];
 document.getElementById("currentDay").innerHTML = day + ", " + month;
 
 
-// saving tasks to local storage
-const textInput = document.getElementById("textInput").value;
-const textOutput = document.getElementsByClassName("textOutput");
-const btn = document.getElementById("btn");
 
+//saving tasks to local storage
+let task14 = document.querySelector("#task14");
 
-btn.onclick = function () {
-  const textInput = textInput.value;
-  const textOutput = textOutput.value;
+task14.value =localStorage.getItem("taskItem")
 
-  console.log(textInput);
-  console.log(textOutput);
-  
-  localStorage.setItem(textInput);
-  location.reload();
-
-};
-
-for (let i = 0; i < localStorage.length; i++) {
-  localStorage.textInput(i);
-  localStorage.getItem(textInput);
-
-  textOutput.innerHTML += '${textInput}';
-
-
-}
-
-
-//Test
-
-const inpKey = document.getElementById("inpKey").value;
-const inpValue = document.getElementById("textValue").value;
-const btnInsert = document.getElementById("btnInsert").value;
-const lsOutput = document.getElementById("lsOutput").value;
-
-btnInsert.onclick = function () {
-  const key = inpKey.value;
-  const value = inpValue.value;
-
-  console.log(key);
-  console.log(value);
-
-};
-
+task14.addEventListener('click', event => {
+  localStorage.setItem("taskItem", event.target.value)
+})
 
